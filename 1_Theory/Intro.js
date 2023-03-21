3b259e2f4b23ef88618863a559ad532847ed23ec
SachinJambhale <113876224+SachinJambhale@users.noreply.github.com>
2023-03-21 23:42:59 +0530

    Add files via upload

---
+++ 1_Theory/Intro.js
@@ -0,0 +1,56 @@
+
+
+
+// Nodejs?
+// Node.js is an open-source, cross-platform, back-end JavaScript runtime 
+// environment that runs on the V8 engine and executes JavaScript code 
+// outside a web browser, which was designed to build scalable network 
+// applications. 
+
+// LTS - Long Term Support 
+
+// even versions 
+// 8, 10, 12, 14, 16, 18
+
+// There are some limitations for the javascript if executed in the web browser
+// like files operations are not allowed. 
+
+// The network application(server) cannot be executed using web browsers  because 
+// those apps needs to be running on a specifc port number to receive the requsts 
+
+// You can overcome these limitations by executing javascript using nodejs 
+
+// THe nodejs comes with some built in libraries(packages). 
+// All the built packages are already installed along with nodejs
+
+
+// THere are plenty of third party packages developed by individual developers 
+// and companies. 
+// You can use those third packages in your project. 
+
+// Nodejs provides a tool called Node Package Manager (NPM) to install/update/uninstall 
+// the third party packages. 
+
+
+// 'yarn' is the advanced alternative to NPM 
+
+
+
+// Nodejs Project:- 
+// Any nodejs based project (Reactjs/Angular/server) requires a project 
+// configuration file called 'package.json'
+
+// To create the package.json file you can use 'npm init' command
+
+// 'npm init -y' here -y represents all yes, to select default configuration 
+
+
+// Learning Nodejs :- 
+//   You need to lern the built in nodejs packages and some of the third party 
+//   packages. 
+//     - file operations 
+//     - data base operations 
+//     - http operations 
+//     - etc. 
+
+    
